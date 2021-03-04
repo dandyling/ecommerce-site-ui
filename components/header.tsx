f85bdbcc7c6326f404e4c7a14070ae50ff232dfe
Dandy Ling <dandyling@gmail.com>
2021-03-04 20:45:53 +0800

    refactor: :art: Refactor header to it's own component

---
+++ components/header.tsx
@@ -0,0 +1,83 @@
+import { faLock, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faHeart, faUser } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from "./header.module.scss";
+
+export const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.toolbar}>
+        <ul className={styles.toolbar__list}>
+          <li className={styles.toolbar__item}>
+            <a href="#">Client Services</a>
+          </li>
+          <li className={styles.toolbar__item}>
+            <a href="#">FAQ</a>
+          </li>
+          <li className={styles.toolbar__item}>
+            <a href="#">About</a>
+          </li>
+          <li className={styles.toolbar__item}>
+            <a href="#">Eng</a>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.middlebar}>
+        <div className={styles.middlebar__left}>
+          <h1 className={styles.middlebar__item}>Blizoo.</h1>
+          <FontAwesomeIcon className={styles.middlebar__icon} icon={faSearch} />
+        </div>
+        <div className={styles.middlebar__right}>
+          <a className={styles.middlebar__item} href="#">
+            Man's'
+          </a>
+          <a className={styles.middlebar__item} href="#">
+            Women's'
+          </a>
+          <FontAwesomeIcon className={styles.middlebar__icon} icon={faHeart} />
+          <FontAwesomeIcon className={styles.middlebar__icon} icon={faUser} />
+          <button className={styles.middlebar__button}>
+            <FontAwesomeIcon className={styles.middlebar__icon} icon={faLock} />
+            Cart (0)
+          </button>
+        </div>
+      </div>
+      <nav className={styles.nav}>
+        <ul className={styles.nav__list}>
+          <li className={styles.nav__item}>
+            <a href="#">Top</a>
+          </li>
+          <li className={styles.nav__item}>
+            <a href="#">Bottom</a>
+          </li>
+          <li className={styles.nav__item}>
+            <a href="#">Swim wear</a>
+          </li>
+          <li className={styles.nav__item}>
+            <a href="#">Watch</a>
+          </li>
+          <li className={styles.nav__item}>
+            <a href="#">Shoes</a>
+          </li>
+          <li className={styles.nav__item}>
+            <a href="#">Bag</a>
+          </li>
+          <li className={styles.nav__item}>
+            <a href="#">Accessories</a>
+          </li>
+          <li className={styles.nav__item}>
+            <a href="#">Sports</a>
+          </li>
+          <li className={styles.nav__item}>
+            <a href="#">Cosmetic</a>
+          </li>
+          <li className={styles.nav__item}>
+            <a href="#">Muslim Wear</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
-import { faLock, faSearch } from "@fortawesome/free-solid-svg-icons";
-import { faHeart, faUser } from "@fortawesome/free-regular-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Head from "next/head";
 import styles from "../styles/Home.module.scss";
+import Header from "./../components/header";
 
 export default function Home() {
   return (
@@ -11,87 +9,7 @@ export default function Home() {
         <title>Blizoo. Ecommerce</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header className={styles.header}>
-        <div className={styles.toolbar}>
-          <ul className={styles.toolbar__list}>
-            <li className={styles.toolbar__item}>
-              <a href="#">Client Services</a>
-            </li>
-            <li className={styles.toolbar__item}>
-              <a href="#">FAQ</a>
-            </li>
-            <li className={styles.toolbar__item}>
-              <a href="#">About</a>
-            </li>
-            <li className={styles.toolbar__item}>
-              <a href="#">Eng</a>
-            </li>
-          </ul>
-        </div>
-        <div className={styles.middlebar}>
-          <div className={styles.middlebar__left}>
-            <h1 className={styles.middlebar__item}>Blizoo.</h1>
-            <FontAwesomeIcon
-              className={styles.middlebar__icon}
-              icon={faSearch}
-            />
-          </div>
-          <div className={styles.middlebar__right}>
-            <a className={styles.middlebar__item} href="#">
-              Man's'
-            </a>
-            <a className={styles.middlebar__item} href="#">
-              Women's'
-            </a>
-            <FontAwesomeIcon
-              className={styles.middlebar__icon}
-              icon={faHeart}
-            />
-            <FontAwesomeIcon className={styles.middlebar__icon} icon={faUser} />
-            <button className={styles.middlebar__button}>
-              <FontAwesomeIcon
-                className={styles.middlebar__icon}
-                icon={faLock}
-              />
-              Cart (0)
-            </button>
-          </div>
-        </div>
-        <nav className={styles.nav}>
-          <ul className={styles.nav__list}>
-            <li className={styles.nav__item}>
-              <a href="#">Top</a>
-            </li>
-            <li className={styles.nav__item}>
-              <a href="#">Bottom</a>
-            </li>
-            <li className={styles.nav__item}>
-              <a href="#">Swim wear</a>
-            </li>
-            <li className={styles.nav__item}>
-              <a href="#">Watch</a>
-            </li>
-            <li className={styles.nav__item}>
-              <a href="#">Shoes</a>
-            </li>
-            <li className={styles.nav__item}>
-              <a href="#">Bag</a>
-            </li>
-            <li className={styles.nav__item}>
-              <a href="#">Accessories</a>
-            </li>
-            <li className={styles.nav__item}>
-              <a href="#">Sports</a>
-            </li>
-            <li className={styles.nav__item}>
-              <a href="#">Cosmetic</a>
-            </li>
-            <li className={styles.nav__item}>
-              <a href="#">Muslim Wear</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
+      <Header />
       <main className={styles.main}>Aws</main>
 
       <footer className={styles.footer}>
